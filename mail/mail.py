e54c74b3b7e1aa9add4eb38e02f4d8ccd06f5ccf
00Mugen00 <johncarlo.purihin@gmail.com>
2018-01-07 22:44:53 +0100

    Mail añadido

---
+++ mail/mail.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from google.appengine.api import mail
+
+#documentación: https://cloud.google.com/appengine/docs/standard/python/refdocs/google.appengine.api.mail#google.appengine.api.mail.EmailMessage
+def send_mail(to,subject,msg):
+    if to:
+        try:
+            message = mail.EmailMessage()
+            # Este método de momento no va a funcionar porque sender tiene que ser el usuario que sube el proyecto
+            # Consulte la siguiente página para más info: https://cloud.google.com/appengine/docs/standard/python/mail/
+            message.sender = 'xagendamlg@gmail.com'
+            message.to = to
+            message.subject = subject
+            message.body = msg
+            # chech_initialized asegura que los destinatarios han sido especificados
+            message.check_initialized()
+            message.send()
+        except mail.MissingRecipientError:
+            print 'No recipients was found'
