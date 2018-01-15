fb8035b673b0b97761319044fb82f3c33576da38
00Mugen00 <johncarlo.purihin@gmail.com>
2018-01-15 18:09:35 +0100

    Categoria refactorizado

--- handlers/categoria.py
+++ handlers/categoria.py
@@ -7,6 +7,7 @@ from util.json import to_json
 from tokens import get_user_from_token
 from models import Categoria
 from facades import categoria as cat
+from facades import AgendamlgNotFoundException
 
 class CategoriaHandler(BaseHandler):
     def get(self,categoria_key):
@@ -35,8 +36,14 @@ class PreferenciasHandler(BaseHandler):
 
     def delete(self,categoria_key):
         _, usuario_sesion = get_user_from_token(self.request.headers.get('bearer'))
-        cat.eliminar_preferencia_usuario(usuario_sesion, ndb.Key(urlsafe=categoria_key))
-        self.response.write(u'{"deleted": true}')
+        try:
+            k = ndb.Key(urlsafe=categoria_key)
+            if not k.get():
+                raise AgendamlgNotFoundException.categoria_no_existe(categoria_key)
+            cat.eliminar_preferencia_usuario(usuario_sesion, k)
+            self.response.write(u'{"deleted": true}')
+        except Exception:
+            raise AgendamlgNotFoundException.categoria_no_existe(categoria_key)
 
     def post(self):
         _, usuario_sesion = get_user_from_token(self.request.headers.get('bearer'))
